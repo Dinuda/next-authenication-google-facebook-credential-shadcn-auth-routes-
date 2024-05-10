f56f1c0122a015894ca884b0bb42f7c052433c85
Dinuda <ydinuda@gmail.com>
2024-05-10 20:36:48 +0530

    chore: sb

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs"
 const config: StorybookConfig = {
-  stories: ["../components/**/*.stories.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../components/**/*.stories.mdx", "../components/ui/**/*.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/nextjs",
